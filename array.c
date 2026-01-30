0b384446dd3154062921453d365c7d97fa690331
RiddhiGupta-code <riddhigupta7002@gmail.com>
2026-01-30 14:02:14 +0000

    addtion of array

--- array.c
+++ array.c
@@ -2,14 +2,21 @@
 void main()
 {
     int i;
-    int a[10];
-    printf("enter array elements");
-    for(i=0;i<=9;i++)
+    int a[3],b[3],c[3];
+    printf("enter array elements of a:\n");
+    printf("enter array elements of b:\n");
+    for(i=0;i<3;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<=9;i++)
+    for(i=0;i<3;i++)
     {
-        printf("%d",a[i]);
+        scanf("%d",&b[i]);
+    }
+    printf("Sum array c:\n");
+    for(i=0;i<3;i++)
+    {
+        c[i]=a[i]+b[i];
+        printf("%d",c[i]);
     }
 }
