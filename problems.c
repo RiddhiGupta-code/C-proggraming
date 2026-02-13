a78b87e072142efcb2607f08a2a04a1972b97648
RiddhiGupta-code <riddhigupta7002@gmail.com>
2026-02-13 05:52:14 +0000

    armstrong number

---
+++ problems.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int num,digit,temp,sum;
+    printf("enter num");
+    scanf("%d",&num);
+    temp=num;
+    sum=0;
+    while(temp!=0)
+    {
+        digit=temp%10;
+        sum=sum+(digit*digit*digit);
+        temp=temp/10;
+    }
+    if(sum==num)
+    {
+        printf("num is armstrong");
+    }
+    else
+    {
+        printf("num is not armstrong");
+    }
+}
