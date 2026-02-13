c45406b5165bf35e26868bc88180fbda7a51c7a8
RiddhiGupta-code <riddhigupta7002@gmail.com>
2026-02-13 06:22:22 +0000

    reverse numbers

--- problems.c
+++ problems.c
@@ -1,45 +1,14 @@
 #include<stdio.h>
 void main()
 {
-   int a,b,ch,i,fact=1;
-   float c,d;
-   int power=1;
-   printf("enter numbers");
-   scanf("%d%d%f%f",&a,&b,&c,&d);
-   printf("enter choice");
-   scanf("%d",&ch);
-   switch(ch)
-   {
-    case 1:
-    printf("addition of two numbers %d",a+b);
-    break;
-    case 2:
-    printf("subtraction of two numbers %d",a-b);
-    break;
-    case 3:
-    printf("multiplication of two numbers %d",a*b);
-    break;
-    case 4:
-    printf("division of two numbers %f",c/d);
-    break;
-    case 5:
-    for(i=1;i<=a;i++)
+    int num,digit;
+    printf("enter num");
+    scanf("%d",&num);
+    while(num>0)
     {
-        fact=fact*i;
+        digit=num%10;
+        printf("%d",digit);
+        num=num/10;
     }
-    printf("factorial is %d",fact);
-    break;
-    case 6:
-    for(i=1;i<=b;i++)
-    {
-        power=power*a;
-
-    }
-    printf("power is %d",power);
-    break;
-    case 7:
-    break;
-    default:
-    printf("wrong choice");
-   }
+    printf("/n");
 }
