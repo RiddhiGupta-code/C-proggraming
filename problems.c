2b8a80733b5e2b949751477a7392a0cc57a62d6f
RiddhiGupta-code <riddhigupta7002@gmail.com>
2026-02-13 06:46:38 +0000

    gcd & scd

--- problems.c
+++ problems.c
@@ -1,14 +1,46 @@
 #include<stdio.h>
 void main()
 {
-    int num,digit;
-    printf("enter num");
-    scanf("%d",&num);
-    while(num>0)
+    int num1,num2,scd,gcd,ch,i;
+    printf("enter num1");
+    printf("enter num2");
+    printf("enter choice");
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    scanf("%d",&ch);
+    num1=abs(num1);
+    num2=abs(num2);
+    switch(ch)
     {
-        digit=num%10;
-        printf("%d",digit);
-        num=num/10;
+        case 1:
+        for(i=2;i<=num1 && i<=num2;i++)
+        {
+            if(num1%i==0 && num2%i==0)
+            {
+                scd=i;
+                break;
+            }
+        }
+        if(scd==-1)
+        {
+            printf("No Common Divisor");
+        }
+        else{
+            printf("scd=%d",scd);
+        }
+        break;
+        case 2:
+        for(i=1;i<=num1 && i<=num2;i++)
+        {
+            if(num1%i==0 && num2%i==0)
+            {
+                gcd=i;
+            }
+        }
+        printf("gcd is %d",gcd);
+        break;
+        default:
+        printf("invalid choice");
     }
-    printf("/n");
+    
 }
